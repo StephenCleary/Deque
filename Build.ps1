10853daf8fd671b7e00ec2ecdd0d01818d0a847f
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-07-09 08:05:46 -0400

    Move to shared build script.

--- Build.ps1
+++ Build.ps1
@@ -1,8 +1,1 @@
-dotnet restore
-cd src
-Get-ChildItem | foreach {
-  cd $_
-  dotnet pack -c Release
-  cd ..
-}
-cd ..
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/452c01f4b81a6c914f2cb8adbc19558e7403a7ff/Build.ps1'))
